840d854c17090fdf440f675d04e3caf6ad4cdafb
Naeim <naeim.smz@gmail.com>
2025-04-22 23:48:22 +0430

    Add dynamic component rendering from skills arra

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
 export default function App() {
   return (
     <div className="card">
@@ -27,6 +60,8 @@ function Intro() {
   );
 }
 
+/*
+
 function SkillList() {
   return (
     <div className="skill-list">
@@ -45,5 +80,30 @@ function Skill({ skill, emoji, color }) {
       <span>{skill}</span>
       <span>{emoji}</span>
     </div>
+  );
+}
+*/
+
+/* Add dynamic component rendering from skills array */
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </ul>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <li className="skill" style={{ backgroundColor: color }}>
+      <span></span>
+      <span>{skill}</span>
+      <span>{level === "beginner" && "👶"}</span>
+      <span>{level === "intermediate" && "🤘"}</span>
+      <span>{level === "advanced" && "👊"}</span>
+    </li>
   );
 }
